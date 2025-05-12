5d0b863d0c08b9f0f5a82a34494738be74677e1a
peisje <darjakovalenko5@gmail.com>
2025-05-12 14:11:56 +0300

    Add files via upload

--- scripts/06_execution_time.py
+++ scripts/06_execution_time.py
@@ -14,21 +14,40 @@ For example:
 
 import time
 import random
-
+# 
+# 
+# class ExecutionTime:
+#     def __init__(self):
+#         self.start_time = time.time()
+# 
+#     def duration(self):
+#         return time.time() - self.start_time
+# 
+# 
+# # ---- run code ---- #
+# 
+# 
+# timer = ExecutionTime()
+# sample_list = list()
+# my_list = [random.randint(1, 888898) for num in
+#            range(1, 1000000) if num % 2 == 0]
+# print('Finished in {} seconds.'.format(timer.duration()))
 
 class ExecutionTime:
     def __init__(self):
         self.start_time = time.time()
-
+        
     def duration(self):
         return time.time() - self.start_time
-
-
-# ---- run code ---- #
-
-
+    
 timer = ExecutionTime()
-sample_list = list()
-my_list = [random.randint(1, 888898) for num in
-           range(1, 1000000) if num % 2 == 0]
+sample_list = []
+
+for i in range(1, 1000000):
+    if i % 2 == 0:
+        sample_list.append(random.randint(1, 888898))
+        
+for i in range(10):
+    print("1111")
+
 print('Finished in {} seconds.'.format(timer.duration()))
