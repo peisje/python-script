112c39cbc745953406f8f007908b7b85199efbe1
peisje <darjakovalenko5@gmail.com>
2025-05-14 10:15:09 +0300

    Update 18_zipper.py

--- scripts/18_zipper.py
+++ scripts/18_zipper.py
@@ -3,18 +3,35 @@ from datetime import datetime
 from zipfile import ZipFile
 
 
-# set file name and time of creation
+# # set file name and time of creation
+# today = datetime.now()
+# file_name = 'zipper_' + today.strftime('%Y.%m.%dh%H%M') + '.zip'
+# dir_name = 'tmp/'  # update path
+# 
+# 
+# def zipdir(path, zip):
+#     for root, dirs, files in os.walk(path):
+#         for file in files:
+#             zip.write(os.path.join(root, file))
+# 
+# if __name__ == '__main__':
+#     zipfile = ZipFile(file_name, 'w')
+#     zipdir(dir_name, zipfile)
+#     zipfile.close()
+
+
+
 today = datetime.now()
 file_name = 'zipper_' + today.strftime('%Y.%m.%dh%H%M') + '.zip'
 dir_name = 'tmp/'  # update path
 
-
-def zipdir(path, zip):
-    for root, dirs, files in os.walk(path):
+def add_folder_to_zip(folderPath,zipFIle):
+    for root,dirs,files in os.walk(folderPath):
         for file in files:
-            zip.write(os.path.join(root, file))
-
-if __name__ == '__main__':
-    zipfile = ZipFile(file_name, 'w')
-    zipdir(dir_name, zipfile)
-    zipfile.close()
+            print(file)
+            print(root)
+            full_path=os.path.join.join(root,file)
+            zipFile.write(full_path)
+        
+with ZipFile("test_zip.zip","w") as my_zip:
+    add_folder_to_zip("Desktop",my_zip)
